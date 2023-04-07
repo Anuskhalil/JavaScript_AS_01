879246591fe0bd815641daade1d6cdd71c18b7e8
Anus khalil <117922339+Anuskhalil@users.noreply.github.com>
2023-04-07 18:42:26 +0500

    Add files via upload

---
+++ Assignment 01/assign.js
@@ -0,0 +1,157 @@
+// let userAge = prompt("What is Your Age?");
+
+// if(userAge <= 10){
+//     console.log("You are Welcome");
+// }
+// else{
+//     console.log("You are not allowed here");
+// }
+
+
+// let table = 5;
+
+// for(let i = 1 ; i <=10 ; i++){
+//     console.log(table + " X " + i + " = "+ table*i);
+// }
+
+                    // Assignment: 01 Start From Here
+
+// Q no: 01
+// let userNumber1 = prompt("Enter first Number");
+// let userNumber2 = prompt("Enter Second Number");
+
+// if(userNumber1 > userNumber2){
+//     console.log(userNumber1);
+// }
+// else{
+//     console.log(userNumber2);
+// }
+
+
+// Q no: 02
+// let writeNumber = prompt("Enter Number");
+
+
+// if(writeNumber > 0 ){
+//     console.log("Number is Positive");
+// }
+// else{
+//     console.log("Number is Negative");
+// }
+
+// Q no: 03
+
+// let one = prompt("Enter Number One");
+// let two = prompt("Enter Number Two");
+// let three = prompt("Enter Number Three");
+// let four = prompt("Enter Number Four");
+// let five = prompt("Enter Number Five");
+
+// if (one>two && one>three && one>four && one>five) {
+//     console.log(one);
+// }
+// else if (two>one && two>three && two>four && twofve) {
+//     cosle.log(two)
+// }
+
+// else if (three>one && three>two && three>four && three>five) {
+//     console.log(three);
+// }
+
+// else if (four>one && four>two && four>three && four>five) {
+//     console.log(four);
+// }
+
+// else{
+//     console.log(five);
+// }
+
+
+// Q no: 04
+
+// Even and Odd for Loop
+// for(let i = 0 ; i <= 15 ; i++){
+
+//     if(i === 0){
+//         console.log(i + " is Even");
+//     }
+
+//     else if(i % 2 === 0){
+//         console.log(i + " is Even");
+//     }
+//     else{
+//         console.log(i + " is Odd");
+//     }
+
+// }
+
+
+// Q no: 05
+
+// let studentGrade = prompt("Enter Your Percentage");
+
+// if (studentGrade < 60){
+//     console.log("Grade : F");      
+//     } 
+//   else if (studentGrade < 70) {
+//           console.log("Grade : D"); 
+//             } 
+//   else if (studentGrade < 80) 
+//        {
+//           console.log("Grade : C"); 
+//   } else if (studentGrade < 90) {
+//           console.log("Grade : B"); 
+
+//   } else if (studentGrade < 100) {
+//           console.log("Grade : A"); 
+// }
+
+// Qno: 06
+
+for (let i = 1 ; i <= 100 ; i++){
+   
+    
+    if(i%3 === 0 && i%5 === 0){
+        console.log(i + " FizzBuzz");
+    }
+
+    else if(i % 3 === 0){
+        console.log(i + " Fizz");
+    }
+
+    else if(i % 5 === 0){
+        console.log(i + " Buzz");
+    }
+
+    else{
+        console.log(i);
+    }
+
+
+}
+
+
+
+// Q no: 07
+
+// let first,second,third;
+// for(first=1; first <=6; first++)
+// {
+//    for (second=1 ; second < first ; second++)
+//      {
+//     third= third+("*");        
+//       }
+//  console.log(third);
+//  third='Here is Nested for loop ';    
+// }
+
+
+
+
+
+
+
+
+
+
+
